ffb69103b104229590235f9b5b2da011aa3a0c39
teitonOrange <nicolas.gonzalez@alumnos.ucn.cl>
2024-12-14 19:35:33 -0300

    feat: add careers module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { CareerModule } from './career/career.module';
 
 @Module({
   imports: [    
-    AuthModule,ConfigModule.forRoot(),
+    AuthModule,ConfigModule.forRoot(), CareerModule,
   ]
 })
 export class AppModule {}
