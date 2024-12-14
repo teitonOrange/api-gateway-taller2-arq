36242b28df495651b0945ed85a5a7138236eea0d
teitonOrange <nicolas.gonzalez@alumnos.ucn.cl>
2024-12-14 19:38:13 -0300

    fix: bugfixes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3001);
+  const app1 = await NestFactory.create(AppModule);
+  app1.enableCors();
+  await app1.listen(process.env.PORT ?? 3001);
+
+  // const app2 = await NestFactory.createMicroservice<MicroserviceOptions>
+  // (AppModule, {
+  //   transport: Transport.GRPC,
+  //     options: {
+  //       package: 'career',
+  //       protoPath: [
+  //         join(__dirname, 'career/career.proto'),
+  //       ],
+  //       url: localhost:5000,
+  //     },
+  //   },
+  // );
 }
 bootstrap();
